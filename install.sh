f5a73e9cd39aa03d7de7d7cc87383c9d8dfb4f97
Olivier Mauras <olivier@mauras.ch>
2017-12-15 18:29:10 +0100

    First commit

---
+++ install.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+set -e
+
+PKG_LIST="base-system lvm2 cryptsetup grub-x86_64-efi efibootmgr vim"
+HOSTNAME="dom1.internal"
+KEYMAP="fr_CH"
+TIMEZONE="Europe/Zurich"
+LANG="en_US.UTF-8"
+
+
+# Install requirements
+xbps-install -y -S -f cryptsetup parted lvm2
+
+# Wipe /dev/sda
+dd if=/dev/zero of=/dev/sda bs=1M count=100
+parted /dev/sda mklabel gpt
+parted -a optimal /dev/sda mkpart primary 2048s 100M
+parted -a optimal /dev/sda mkpart primary 100M 1100M
+parted -a optimal /dev/sda mkpart primary 1100M 100%
+parted /dev/sda set 1 boot on
+
+# Encrypt /dev/sda3 partition
+cryptsetup luksFormat -c aes-xts-plain64 -s 512 /dev/sda3
+cryptsetup luksOpen /dev/sda3 crypt-pool
+
+# Now create VG
+pvcreate /dev/mapper/crypt-pool
+vgcreate vgpool /dev/mapper/crypt-pool
+lvcreate -L 10G -n root vgpool
+lvcreate -L 5G -n var vgpool
+lvcreate -L 512M -n home vgpool
+
+# Format filesystems
+mkfs.vfat /dev/sda1
+mkfs.ext4 -L boot /dev/sda2
+mkfs.ext4 -L root /dev/mapper/vgpool-root
+mkfs.ext4 -L var /dev/mapper/vgpool-var
+mkfs.ext4 -L home /dev/mapper/vgpool-home
+
+# Mount them
+mount /dev/mapper/vgpool-root /mnt
+for dir in dev proc sys boot home var; do
+  mkdir /mnt/${dir}
+done
+
+mount /dev/mapper/vgpool-home /mnt/home
+mount /dev/mapper/vgpool-var /mnt/var
+
+mount /dev/sda2 /mnt/boot
+mkdir /mnt/boot/efi
+
+mount /dev/sda1 /mnt/boot/efi
+
+for fs in dev proc sys; do
+  mount -o bind /${fs} /mnt/${fs}
+done
+
+# Now install void
+xbps-install -y -S -R http://repo.voidlinux.eu/current -r /mnt $PKG_LIST
+
+# Do a bit of customization
+echo "[!] Setting root password"
+passwd -R /mnt root
+echo $HOSTNAME > /mnt/etc/hostname
+echo "TIMEZONE=${TIMEZONE}" >> /mnt/etc/rc.conf
+echo "KEYMAP=${KEYMAP}" >> /mnt/etc/rc.conf
+echo "TTYS=2" >> /mnt/etc/rc.conf
+
+
+echo "LANG=$LANG" > /mnt/etc/locale.conf
+echo "$LANG $(echo ${LANG} | cut -f 2 -d .)" >> /mnt/etc/default/libc-locales
+chroot /mnt xbps-reconfigure -f glibc-locales
+
+FSTAB="/mnt/etc/fstab"
+echo "LABEL=root    /	ext4	rw,relatime,data=ordered,discard	0 0" >> $FSTAB
+echo "LABEL=boot    /boot	ext4	rw,relatime,data=ordered,discard	0 0" >> $FSTAB
+echo "LABEL=var /var	ext4	rw,relatime,data=ordered,discard	0 0" >> $FSTAB
+echo "LABEL=home    /home	ext4	rw,relatime,data=ordered,discard	0 0" >> $FSTAB
+echo "/dev/sda1 /boot/efi	vfat	defaults	0 0" >> $FSTAB
+echo "tmpfs     /tmp	tmpfs	size=1G,noexec,nodev,nosuid	0 0" >> $FSTAB
+
+rm -rf /mnt/var/tmp
+ln -s /tmp /mnt/var/tmp
+
+chroot /mnt grub-install /dev/sda
+
+# Now tune the cryptsetup
+KERNEL_VER=$(xbps-query -r /mnt -s linux4 | cut -f 2 -d ' ' | cut -f 1 -d -)
+echo "KERNEL_VER IS ${KERNEL_VER}"
+
+echo -e "crypt-pool\t/dev/sda3\tnone\tluks" > /mnt/etc/crypttab
+mkdir -p /mnt/etc/dracut.conf.d/
+echo 'install_items+="/etc/crypttab"' > /mnt/etc/dracut.conf.d/00-crypttab.conf
+echo 'hostonly=yes' > /mnt/etc/dracut.conf.d/00-hostonly.conf
+
+echo 'GRUB_CRYPTODISK_ENABLE=y' >> /mnt/etc/default/grub
+echo "GRUB_CMDLINE_LINUX=\"rd.auto=1 rd.vconsole.keymap=${KEYMAP} cryptsetup=/dev/sda3:crypt-pool\"" >> /mnt/etc/default/grub
+
+chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+chroot /mnt xbps-reconfigure -f ${KERNEL_VER}
+
